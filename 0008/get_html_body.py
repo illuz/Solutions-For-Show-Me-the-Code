baba37b7ef5d831904c6a2378fc21b8ac1677b00
illuz <iilluzen@gmail.com>
2015-03-15 21:44:20 +0800

    fix 0008

--- 0008/get_html_body.py
+++ 0008/get_html_body.py
@@ -5,15 +5,21 @@
 # Create Date: 2015-03-15 21:07:33
 # Usage:       get_html_body.py url
 
+"""
+第 0008 题：一个HTML文件，找出里面的正文。
+"""
+
 import sys
 from pyquery import PyQuery as pq
 
 def get_html_body(url):
     page = pq(url)
-    print page('body').html().encode('utf-8', 'ignore')
+    for item in page('body p'):
+        print page(item).text().encode('utf-8', 'ignore')
+    # print page('body p').text().encode('utf-8', 'ignore')
 
 def main():
-    # get_html_body('http://mooc.guokr.com/post/610231/')
+    get_html_body('http://mooc.guokr.com/post/610231/')
     if len(sys.argv) != 2:
         print("Need exact 1 parameter.")
     else:
